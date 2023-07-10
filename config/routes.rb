32897567ac6c5d2bf7fc24fbb368eee3b989bce7
Tracy-miranja <barazatracy44@gmail.com>
2023-07-10 19:40:13 +0300

    Add splash page

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'users/destroy'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root to: "users#index"
+  root 'splash#index'
   # Defines the root path route ("/")
   # root "articles#index"
 end
