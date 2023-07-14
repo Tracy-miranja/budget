130a6d44101952852dfac6d77e2802da1a6d83c2
Tracy-miranja <barazatracy44@gmail.com>
2023-07-14 23:28:48 +0300

    Add test to my project

--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -1,7 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe "Categories", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  let(:user) { instance_double(User) }
+
+  describe "GET /categories" do
+
+      it "displays the category's name and total amount" do
+      end
+    end
+
+  describe "POST /categories" do
+    context "with valid attributes" do
+      before do
+        allow(user).to receive(:categories).and_return([])
+        allow(controller).to receive(:current_user).and_return(user)
+      end
+
+      it "creates a new category" do
+      end
+    end
+
+    context "with invalid attributes" do
+      before do
+        allow(user).to receive(:categories).and_return([])
+        allow(controller).to receive(:current_user).and_return(user)
+      end
+
+      it "does not create a new category" do
+      end
+    end
+  end
+
+  describe "DELETE /categories/:id" do
+    before do
+      allow(user).to receive(:categories).and_return([])
+      allow(controller).to receive(:current_user).and_return(user)
+    end
+
+    it "destroys the category" do
+    end
   end
 end
