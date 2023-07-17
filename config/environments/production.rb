d89e0b098f1b8ddf83ec3e1b5d67173fce7a0265
Tracy-miranja <barazatracy44@gmail.com>
2023-07-17 18:45:22 +0300

    Correct linter error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,6 +91,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  Rails.application.routes.default_url_options[:host] = 'Tracy.oregon.render.com'
+  Rails.application.routes.default_url_options[:host] = ENV['HOST']
 
 end
